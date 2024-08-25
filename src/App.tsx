c8725c1f66ca0fcc5278319c39ee6640159a52ab
nqlp <nqlpaul@gmail.com>
2024-08-24 21:50:58 -0400

    chore: Add footer component and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Hero from './pages/hero/Hero';
 import Header from './components/header/Header';
+import Footer from './components/footer/Footer';
 
 function App() {
 
@@ -7,6 +8,7 @@ function App() {
     <>
       <Header />
       <Hero />
+      <Footer />
     </>
   )
 }
