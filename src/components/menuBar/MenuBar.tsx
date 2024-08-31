070eb93a7cf9e3f9a2f39fba2e14dcd64366d692
nqlp <nqlpaul@gmail.com>
2024-08-30 20:08:44 -0400

    chore: Update menu bar styling and add navigation and social icons

--- src/components/menuBar/MenuBar.tsx
+++ src/components/menuBar/MenuBar.tsx
@@ -1,9 +1,28 @@
 import './MenuBar.css';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 function MenuBar() {
   return (
     <div className="menu-bar">
       <h1>Menu Bar</h1>
+
+      <div className="nav-bar">
+        Accueil <br />
+        Nous <br />
+        Portfolio <br />
+        Approche <br />
+        Contact <br />
+      </div>
+
+      <div className="socials">
+        <FacebookIcon /> <br />
+        <InstagramIcon /> <br />
+        <XIcon /> <br />
+        <LinkedInIcon /><br />
+      </div>
     </div>
   );
 }
