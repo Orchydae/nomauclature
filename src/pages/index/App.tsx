75ad4b291211d6a7902d95910329042859181e2b
David <nguyen.dddavid@hotmail.com>
2024-09-03 22:40:12 -0400

    chore: Update npm dependencies and add framer-motion library

--- src/pages/index/App.tsx
+++ src/pages/index/App.tsx
@@ -4,26 +4,33 @@ import Hero from './hero/Hero';
 import Footer from '../../components/footer/Footer';
 import ProfileCard from '../../components/profileCard/ProfileCard';
 
-import { useRef } from 'react';
+import React, { useRef, Suspense } from 'react';
 import { Parallax } from 'react-scroll-parallax';
-import {motion, useScroll} from 'framer-motion';
+import { useScroll } from 'framer-motion';
+import { Canvas } from '@react-three/fiber';
+import { Environment, OrbitControls } from '@react-three/drei';
+import GlassCrane from '../../../public/Glass-crane';
 
-import davidAvatar from '../../assets/david-avatar-champagne-.png';
-import paulAvatar from '../../assets/paul-avatar-heart-.png';
 import danielOlah from '../../assets/daniel-olah-unsplash.jpg';
 
-import {members} from '../../data/members';
+import { members } from '../../data/members';
+
+const Scene = React.lazy(() => import('../../components/scene/Scene'));
 
 function App() {
 
   const container = useRef(null);
-  const {scrollYProgress} = useScroll({
+  const { scrollYProgress } = useScroll({
     target: container,
     offset: ['start start', 'end end'],
   })
 
   return (
     <main className="main-container">
+      <Suspense fallback={<div>Loading...</div>}>
+        <Scene />
+      </Suspense>
+
       <VideoPlayer
         className="hero-video"
         src={lavaLamp}
@@ -57,11 +64,17 @@ function App() {
           members.map((member: any, index: any) => {
             const targetScale = 1 - ((members.length - index) * 0.05);
             return (
-              <ProfileCard key={index} i={index} {...member} progress={scrollYProgress} range={[index * 0.25, 1]} targetScale={targetScale}/>
+              <ProfileCard key={index} i={index} {...member} progress={scrollYProgress} range={[index * 0.25, 1]} targetScale={targetScale} />
             )
           })
         }
       </section>
+
+      <section className="work-section">
+        <div className="title">✦ Nos réalisations</div>
+
+      </section>
+
       <Footer />
 
     </main>
