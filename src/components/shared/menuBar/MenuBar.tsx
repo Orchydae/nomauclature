4dec09a1af7fbaa76b8e87fbc947cac5688b2258
nqlp <nqlpaul@gmail.com>
2024-09-15 23:24:31 -0400

    Refactor MenuBar component and add routing to Contact page

--- src/components/shared/menuBar/MenuBar.tsx
+++ src/components/shared/menuBar/MenuBar.tsx
@@ -3,18 +3,17 @@ import FacebookIcon from '@mui/icons-material/Facebook';
 import InstagramIcon from '@mui/icons-material/Instagram';
 import XIcon from '@mui/icons-material/X';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import { Link } from 'react-router-dom';
 
 function MenuBar() {
   return (
-    <div className="menu-bar">
-      <h1>Menu Bar</h1>
-
-      <div className="nav-bar">
-        Accueil <br />
-        Nous <br />
-        Portfolio <br />
-        Approche <br />
-        Contact <br />
+    <div>
+      <div>
+        <Link to="/" className="links">Accueil</Link> <br />
+        {/* Nous <br /> */}
+        {/* Portfolio <br />
+        Approche <br /> */}
+        <Link to="/contact" className="links">Contact</Link>
       </div>
 
       <div className="socials">
@@ -22,6 +21,8 @@ function MenuBar() {
         <InstagramIcon /> <br />
         <XIcon /> <br />
         <LinkedInIcon /><br />
+      </div>
+      <div>
       </div>
     </div>
   );
