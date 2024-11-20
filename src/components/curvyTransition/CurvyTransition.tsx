3278d108f0a65dc5000c199c869f25842eada3c5
David <nguyen.dddavid@hotmail.com>
2024-11-19 21:44:42 -0500

    FEAT: Added curvy transition

--- src/components/curvyTransition/CurvyTransition.tsx
+++ src/components/curvyTransition/CurvyTransition.tsx
@@ -1,7 +1,41 @@
+import styles from './curvy-transition.module.css';
+
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useRef, useEffect } from 'react';
+
+gsap.registerPlugin(ScrollTrigger);
+
 function CurvyTransition() {
+    const circleContainerRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        const circleContainer = circleContainerRef.current;
+        if (!circleContainer) return;
+
+        ScrollTrigger.create({
+            trigger: circleContainer,
+            start: "top bottom",
+            end: "bottom top",
+            scrub: 1,
+            onUpdate: (self) => {
+                const height = 50 - (self.progress * 50);
+                console.log(height);
+                gsap.set(circleContainer, { 
+                    height: `${height}px`, 
+                    overwrite: true 
+                });
+            },
+        });
+
+        return () => {
+            ScrollTrigger.getAll().forEach(instance => instance.kill());
+        }
+    })
+
     return(
-        <div>
-            Curvy
+        <div ref={circleContainerRef} className={styles.circleContainer}>
+            <div className={styles.circle}></div>
         </div>
     )
 }
